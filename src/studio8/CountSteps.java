a775094587c74926b18760675fd0d3c726bf6d42
vppatel <vppatel@wustl.edu>
2018-03-19 15:42:46 -0500

    pls work

--- src/studio8/CountSteps.java
+++ src/studio8/CountSteps.java
@@ -1,7 +1,45 @@
 package studio8;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import com.sun.corba.se.spi.orbutil.fsm.Input;
+
 // TODO: Develop an algorithm to count steps in accelerometer data
 //    Major steeps:
 //       1. Create a class and main method.
 //       2. Using a Scanner and File object, read data from your .csv file.
 //       3. Develop and test algorithms to count the "peaks" in the data.
+
+public class CountSteps {
+
+	public static void main(String[] args) {
+
+		String line = "";
+		String csvSplitBy = ",";
+		double zReadings[] = new double [540];
+
+		File readings = new File("CSE132-studio8/data/readings2.csv");
+		try {
+			Scanner sc = new Scanner(readings).useDelimiter(csvSplitBy);
+			while ((sc.hasNextDouble()) == true) {
+				for (int i = 0; i < zReadings.length; i = i + 3) {
+					double x = sc.nextDouble();
+					double y = sc.nextDouble();
+					double z = sc.nextDouble();
+					zReadings[i] = z; 
+					System.out.println(z);
+				}
+			}
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+
+
+	}
+
+
+}
---
+++ steps/steps.ino
@@ -0,0 +1,30 @@
+#include <Wire.h> // Must include Wire library for I2C
+#include <SparkFun_MMA8452Q.h> // Includes the SFE_MMA8452Q library
+
+
+MMA8452Q accel;
+
+void setup() {
+  Serial.begin(9600);
+  accel.init(); // Default init: +/-2g and 800Hz ODR
+}
+
+
+
+void loop() {
+
+  if (accel.available()) {
+    accel.read();
+    double accelerationX = accel.cx;
+    double accelerationY = accel.cy;
+    double accelerationZ = accel.cz;
+    Serial.print(accelerationX, 3);
+    Serial.print(",");
+    Serial.print(accelerationY, 3);
+    Serial.print(",");
+    Serial.println(accelerationZ, 3);
+  }
+
+
+}
+
